fac32912521d13f70a47e3ddf763626479673749
Master0229 <masterservice2020@gmail.com>
2022-05-03 15:39:03 +0530

    SN_WebUpdate

--- src/app/components/cleanui/system/Auth/lockscreen/lockscreen.component.ts
+++ src/app/components/cleanui/system/Auth/lockscreen/lockscreen.component.ts
@@ -18,7 +18,7 @@ export class LockscreenComponent {
     if (this.users.some(x => x.pin.toString() == pin)) {
       localStorage.setItem("user", JSON.stringify(this.users.filter(x => x.pin.toString() == pin)[0]));
       // this.notification.success("Logged In!", "Logged in successfully")
-      this.router.navigateByUrl('/apps/sale');
+      this.router.navigateByUrl('/apps/dashboard');
     } else {
       this.notification.error("Pin Invalid", "Check your PIN")
     }
